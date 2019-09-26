2b911b7a96d58d4e1265ef8713583bbd2fbf5415
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:31:13 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan15-Logika/src/pbo/ulang/pkg10114504/latihan15/logika/PBOULANG10114504Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program logika true and false
+ */
+public class PBOULANG10114504Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("!a = " + !a );
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
